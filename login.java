7cdc0922c01fbca2b550d03040f369a7ab052a8a
konduruvinodhkumar <kvinu8639@gmail.com>
2025-06-01 09:50:02 +0530

    java code

--- login.java
+++ login.java
@@ -2,14 +2,8 @@ public class login{
 public static void main(string args[])
 {
  int i;
-  for(i=0;i<=25;i++){
+  for(i=0;i<=10;i++){
          system.out.println("Number: "+i)
 }
 }
 }
-gsadj
-avdsdtashda
-avyasyd
-avsdiuads
-bvasdyasyd
-
